5045bebafce35b2799b3276de05b7dacd6b1177a
beginnerpyth <hskx7779@gmail.com>
2026-01-20 19:44:04 +0900

    Add files via upload

---
+++ number.py
@@ -0,0 +1,10 @@
+num = -5
+print(abs(num))
+num = 4
+print(pow(3,2))
+num = 2
+print(max(4,6))
+num = 1
+print(min(1,2))
+num=4
+print(round(3.7))
---
+++ questions.py
@@ -0,0 +1,8 @@
+value = "abc"
+print("a" in value and "b" in value)
+num = 10
+print(type(num))
+n = str(num)
+print((n))
+f = float(num)
+print(type(f))
